76ea6d2c52c2f68ba0f47d64cb717bb7d9bf12e6
pigstar02 <1910156852@qq.com>
2023-07-07 14:40:35 +0800

    project0 task1&task2

--- src/primer/trie_store.cpp
+++ src/primer/trie_store.cpp
@@ -1,48 +1,85 @@
 #include "primer/trie_store.h"
+#include <mutex>
+#include <optional>
 #include "common/exception.h"
 
 namespace bustub {
 
-template <class T>
-auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
-  // Pseudo-code:
-  // (1) Take the root lock, get the root, and release the root lock. Don't lookup the value in the
-  //     trie while holding the root lock.
-  // (2) Lookup the value in the trie.
-  // (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
-  //     root. Otherwise, return std::nullopt.
-  throw NotImplementedException("TrieStore::Get is not implemented.");
-}
+	template <class T>
+	auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<T>> {
+		// Pseudo-code:
+		// (1) Take the root lock, get the root, and release the root lock. Don't lookup the value in the
+		//     trie while holding the root lock.
+		// (2) Lookup the value in the trie.
+		// (3) If the value is found, return a ValueGuard object that holds a reference to the value and the
+		//     root. Otherwise, return std::nullopt.
+		std::unique_lock<std::mutex> lk(root_lock_);
+		Trie root = root_;
+		lk.unlock();
+		const T *result = root.Get<T>(key);
+		if (result == nullptr)
+		{
+			return std::nullopt;
+		}
+		return std::optional<ValueGuard<T>>(ValueGuard<T>(root, std::move(*result)));
+		// throw NotImplementedException("TrieStore::Get is not implemented.");
+	}
 
-template <class T>
-void TrieStore::Put(std::string_view key, T value) {
-  // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
-  // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Put is not implemented.");
-}
+	template <class T>
+	void TrieStore::Put(std::string_view key, T value) {
+		// You will need to ensure there is only one writer at a time. Think of how you can achieve this.
+		// The logic should be somehow similar to `TrieStore::Get`.
+		std::unique_lock<std::mutex> write_lk(write_lock_);
 
-void TrieStore::Remove(std::string_view key) {
-  // You will need to ensure there is only one writer at a time. Think of how you can achieve this.
-  // The logic should be somehow similar to `TrieStore::Get`.
-  throw NotImplementedException("TrieStore::Remove is not implemented.");
-}
+		std::unique_lock<std::mutex> read_lk(root_lock_);
+		Trie root = root_;
+		read_lk.unlock();
 
-// Below are explicit instantiation of template functions.
+		auto result = root.Put(key, std::move(value));
 
-template auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<uint32_t>>;
-template void TrieStore::Put(std::string_view key, uint32_t value);
+		read_lk.lock();
+		root_ = root;
+		read_lk.unlock();
 
-template auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<std::string>>;
-template void TrieStore::Put(std::string_view key, std::string value);
+		write_lk.unlock();
+		// throw NotImplementedException("TrieStore::Put is not implemented.");
+	}
 
-// If your solution cannot compile for non-copy tests, you can remove the below lines to get partial score.
+	void TrieStore::Remove(std::string_view key) {
+		// You will need to ensure there is only one writer at a time. Think of how you can achieve this.
+		// The logic should be somehow similar to `TrieStore::Get`.
+		std::unique_lock<std::mutex> write_lk(write_lock_);
 
-using Integer = std::unique_ptr<uint32_t>;
+		std::unique_lock<std::mutex> read_lk(root_lock_);
+		Trie root = root_;
+		read_lk.unlock();
 
-template auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<Integer>>;
-template void TrieStore::Put(std::string_view key, Integer value);
+		auto result = root.Remove(key);
 
-template auto TrieStore::Get(std::string_view key) -> std::optional<ValueGuard<MoveBlocked>>;
-template void TrieStore::Put(std::string_view key, MoveBlocked value);
+		read_lk.lock();
+		root_ = root;
+		read_lk.unlock();
+
+		write_lk.unlock();
+		throw NotImplementedException("TrieStore::Remove is not implemented.");
+	}
+
+	// Below are explicit instantiation of template functions.
+
+	template auto TrieStore::Get(std::string_view key)->std::optional<ValueGuard<uint32_t>>;
+	template void TrieStore::Put(std::string_view key, uint32_t value);
+
+	template auto TrieStore::Get(std::string_view key)->std::optional<ValueGuard<std::string>>;
+	template void TrieStore::Put(std::string_view key, std::string value);
+
+	// If your solution cannot compile for non-copy tests, you can remove the below lines to get partial score.
+
+	using Integer = std::unique_ptr<uint32_t>;
+
+	template auto TrieStore::Get(std::string_view key)->std::optional<ValueGuard<Integer>>;
+	template void TrieStore::Put(std::string_view key, Integer value);
+
+	template auto TrieStore::Get(std::string_view key)->std::optional<ValueGuard<MoveBlocked>>;
+	template void TrieStore::Put(std::string_view key, MoveBlocked value);
 
 }  // namespace bustub
